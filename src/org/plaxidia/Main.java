8f6c819152d62de9361cba12ed5db119c35f7b50
plaxidia <plaxidiahove@gmail.com>
2021-11-29 20:36:30 +0300

    Added FindAverage( int [] arr) function.

--- src/org/plaxidia/Main.java
+++ src/org/plaxidia/Main.java
@@ -5,4 +5,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+    public static int FindAverage( int [] arr)
+    {
+        //the average is found by dividing the sum with the number of elements
+        int sum  =0;
+        for(int n =1; n  < arr.length ; n++)
+        {
+            sum = sum + arr[n];
+        }
+        System.out.println(" sum " + sum );
+        return sum/arr.length;
+    }
 }
