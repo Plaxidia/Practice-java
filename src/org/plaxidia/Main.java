01790af018550ed953e85be58984823b44870e4f
plaxidia <plaxidiahove@gmail.com>
2021-11-29 20:50:19 +0300

    Added function FindMin (int[] arr).

--- src/org/plaxidia/Main.java
+++ src/org/plaxidia/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int [] num ={8,2,1,-3,124};
         System.out.println("min of 5 numbers is =" + FindMin(num) );
+       	System.out.println("max of 5 numbers is =" + FindMax(num) );
         System.out.println("average of 5 numbers is =" + FindAverage(num) );
     }
     public static int FindAverage( int [] arr)
@@ -18,20 +19,33 @@ public class Main {
         System.out.println(" sum " + sum );
         return sum/arr.length;
     }
-    public static int FindMin (int[] num)
-    {
+    public static int FindMin (int[] arr) {
         int n = 0;
-        int min = num [n];
+        int min = arr[n];
 
-        for ( int i =0;i< num.length ;i++)
-        {
-            if (num[i] < min )
-            {
-                min=num[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
 
             }
         }
         return min;
     }
+        public static int FindMax (int [] arr)
+        {
+            int n = 0;
+            int max = arr[n];
+
+            for( int i =0;i< arr.length ;i++)
+            {
+                if (arr[i] >max)
+                {
+                    max=arr[i];
+                }
+            }
+            return max;
+
+        }
+
 
 }
