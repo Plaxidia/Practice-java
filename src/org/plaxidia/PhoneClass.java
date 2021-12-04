bc14637a746282aa00bae562795fea0783c930d7
plaxidia <plaxidiahove@gmail.com>
2021-12-05 01:03:57 +0300

    declared the veriables. added 2 methods

--- src/org/plaxidia/PhoneClass.java
+++ src/org/plaxidia/PhoneClass.java
@@ -1,4 +1,26 @@
 package org.plaxidia;
 
 public class PhoneClass {
+    private String  number ;
+    private String model;
+    private String weight ;
+    private String  receivecall;
+    private String name ;
+    PhoneClass()
+    {
+
+        System.out.println("NEW PHONE CREATED");
+    }
+
+    PhoneClass(String  n,String iphone ,String grams  )
+    {
+        number =n;
+        model = iphone ;
+        weight =grams;
+        System.out.println("NEW NUMBER :" + n);
+        System.out.println(" Model :" + iphone );
+        System.out.println("weight :" + grams );
+
+    }
+
 }
